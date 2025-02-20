a7087eb49ea3c9b083088152ad4f7f51e621875b
Luis Fernando L. Chagas <lflchagas@gmail.com>
2025-02-20 17:28:47 -0300

    Commit inicial

---
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Api.UseCases.Users.Register;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+
+namespace TechLibrary.Api.Controllers{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Create(RequestUserJson request)
+        {
+            var useCase = new RegisterUserUseCase();
+
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+
+    }
+}
---
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -0,0 +1,15 @@
+﻿using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+
+namespace TechLibrary.Api.UseCases.Users.Register;
+
+public class RegisterUserUseCase
+{
+    public ResponseRegisteredUserJson Execute(RequestUserJson request)
+    {
+        return new ResponseRegisteredUserJson
+        {
+
+        };
+    }
+}
